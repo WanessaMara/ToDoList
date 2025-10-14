b53ecc3ae65b68dfd36be5e25482efa56d10f486
WanessaMara <wanessammlima21@gmail.com>
2025-10-14 07:04:01 -0300

    Primeiro commit do projeto para salvamento dos primeiros arquivos, sendo model e data - API ToDoList .NET 9

---
+++ Models/Tarefa.cs
@@ -0,0 +1,13 @@
+
+namespace ToDoList.Models
+{
+    public class Tarefa 
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty; //Inicializa com string vazia
+        public string? Descricao { get; set; } //Pode ser nulo
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+        public bool Concluida { get; set; } = false; //Inicializa como false
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona serviços
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configuração do banco (exemplo)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=tarefas.db"));
+
+var app = builder.Build();
+
+// Configura o Swagger (interface visual)
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
